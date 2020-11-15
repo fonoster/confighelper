6a67a61cdbc0dfdf8fa9702dfdec4eb4f1f27d52
Pedro Sanders <sanderspedro@gmail.com>
2020-11-15 12:51:48 -0500

    Update config-client.js

--- config-client.js
+++ config-client.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const { createClientConfig } = require('@fonos/certs/dist/certs')
 const endpoint = process.env.ENDPOINT || 'fonos.local'
-createClientConfig('/certs', endpoint)
+createClientConfig('/config', endpoint)

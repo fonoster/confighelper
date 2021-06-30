0e27025f28c438c12de75f253c5cfc49289d81f3
Pedro Sanders <psanders@fonoster.com>
2021-06-29 21:20:18 -0400

    Removing endpoint

--- config-client.js
+++ config-client.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
 const { createClientConfig } = require('@fonos/certs/dist/certs')
-const endpoint = process.env.ENDPOINT || 'fonos.local'
-createClientConfig('/config', endpoint)
+createClientConfig('/config')
